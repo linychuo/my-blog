0745f0a0bab49b39d98831f310234e12cc972f6a
linychuo <linychuo@gmail.com>
2018-08-17 15:44:46 +0800

    refactor code

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,7 @@ const Home = () => (
 		<div class="wrapper">
 			<div>
 				<ul class="post-list">
-					{config.articles.map(article => (
+					{config.articles.fitler(article => article.showOnMain).map(article => (
 						<li>
 							<span class="post-meta">{article.created}</span>
 							<h2><Link class="post-link" to={`/article/${article.name}`}>{article.name}</Link></h2>
@@ -22,29 +22,13 @@ const Home = () => (
 	</div>
 );
 
-const About = () => (
-	<div class="page-content">
-		<div class="wrapper">
-			<div class="post">
-				<header class="post-header">
-					<h1 class="post-title">About</h1>
-				</header>
-
-				<article class="post-content">
-					<p><a href="https://github.com/linychuo">github.com/linychuo</a></p>
-					<p><a href="https://bitbucket.org/linychuo">bitbucket.org/linychuo</a></p>
-				</article>
-			</div>
-		</div>
-	</div>
-);
 
 const App = () => (
 	<MemoryRouter>
 		<div>
 			<Header />
 			<Route exact path="/" component={Home} />
-			<Route path="/about" component={About} />
+			<Route path="/article/about" component={Article} />
 			<Route path="/article/:name" component={Article} />
 			<Footer />
 		</div>
