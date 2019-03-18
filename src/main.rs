658ab1af7fcf17319e54c9c44e5fe1a104a1612e
linychuo <linychuo@gmail.com>
2019-03-18 21:57:10 +0800

    remove unused code

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,30 @@ mod post;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
-    #[structopt(default_value = "./posts")]
-    posts_dir: String,
-    #[structopt(default_value = "./static")]
-    static_files_dir: String,
-    #[structopt(default_value = "./templates")]
-    templates_dir: String,
-    #[structopt(default_value = "./build")]
-    build_dir: String,
-    #[structopt(default_value = "about")]
-    excludes: Vec<String>,
+	#[structopt(default_value = "./posts")]
+	posts_dir: String,
+	#[structopt(default_value = "./static")]
+	static_files_dir: String,
+	#[structopt(default_value = "./templates")]
+	templates_dir: String,
+	#[structopt(default_value = "./build")]
+	build_dir: String,
+	#[structopt(default_value = "about")]
+	excludes: Vec<String>,
 }
 
 fn main() {
-    let args = Cli::from_args();
-    let blog = Blogger::new(&args.build_dir, &args.posts_dir, &args.templates_dir);
+	let args = Cli::from_args();
+	let blog = Blogger::new(&args.build_dir, &args.posts_dir, &args.templates_dir);
 
-    blog.render_posts(&args.excludes)
-        .expect("render all posts failed!");
-    for it in args.excludes {
-        blog.render(it.as_str()).expect("render about file failed!");
-    }
+	blog.render_posts(&args.excludes)
+		.expect("render all posts failed!");
+	for it in args.excludes {
+		blog.render(it.as_str()).unwrap();
+	}
 
-    Blogger::copy_static_files(
-        PathBuf::from(&args.static_files_dir),
-        PathBuf::from(&args.build_dir),
-    );
+	Blogger::copy_static_files(
+		PathBuf::from(&args.static_files_dir),
+		PathBuf::from(&args.build_dir),
+	);
 }
