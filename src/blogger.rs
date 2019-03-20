b13f0bcec7a14e6e375bf2d9cc80a93677fbb276
linychuo <linychuo@gmail.com>
2019-03-20 22:58:56 +0800

    refactoring code

--- src/blogger.rs
+++ src/blogger.rs
@@ -10,156 +10,155 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Blogger {
-    dest_dir: PathBuf,
-    posts_dir: PathBuf,
-    hbs: Handlebars,
-    comrak_options: ComrakOptions,
+	dest_dir: PathBuf,
+	posts_dir: PathBuf,
+	hbs: Handlebars,
+	comrak_options: ComrakOptions,
 }
 
 impl Blogger {
-    pub fn new(dest_dir: &str, posts_dir: &str, template_dir: &str) -> Blogger {
-        let mut hbs = Handlebars::new();
-        hbs.set_strict_mode(true);
-        hbs.register_templates_directory(".hbs", Path::new(template_dir))
-            .expect("register dir of templates failed");
-        fs::create_dir_all(&dest_dir).expect("create dest dir failed");
+	pub fn new(dest_dir: &str, posts_dir: &str, template_dir: &str) -> Blogger {
+		let mut hbs = Handlebars::new();
+		hbs.set_strict_mode(true);
+		hbs.register_templates_directory(".hbs", Path::new(template_dir))
+			.expect("register dir of templates failed");
+		fs::create_dir_all(&dest_dir).expect("create dest dir failed");
 
-        Blogger {
-            dest_dir: PathBuf::from(dest_dir),
-            posts_dir: PathBuf::from(posts_dir),
-            hbs,
-            comrak_options: ComrakOptions {
-                ext_header_ids: Some("".to_string()),
-                ..ComrakOptions::default()
-            },
-        }
-    }
+		Blogger {
+			dest_dir: PathBuf::from(dest_dir),
+			posts_dir: PathBuf::from(posts_dir),
+			hbs,
+			comrak_options: ComrakOptions {
+				ext_header_ids: Some("".to_string()),
+				..ComrakOptions::default()
+			},
+		}
+	}
 
-    pub fn render_posts(&self, exclude: &[String]) -> Result<(), RenderError> {
-        let mut all_posts = self.load_posts(exclude)?;
-        all_posts.sort_by_key(|post| post.created_date_time.clone());
-        all_posts.reverse();
+	pub fn render_posts(&self, exclude: &[String]) -> Result<(), RenderError> {
+		let mut all_posts = self.load_posts(exclude)?;
+		all_posts.sort_by_key(|post| post.created_date_time.clone());
+		all_posts.reverse();
+		self.render_other("index", &json!({"parent": "layout", "posts": all_posts}))?;
 
-        self.render_other("index", &json!({"parent": "layout", "posts": all_posts}))?;
-        let mut post_by_tag: HashMap<String, Vec<Post>> = HashMap::new();
-        for item in all_posts {
-            for tag in &item.tags {
-                if !post_by_tag.contains_key(tag) {
-                    post_by_tag.insert(tag.to_string(), vec![]);
-                }
-                post_by_tag.get_mut(tag).unwrap().push(item.clone());
-            }
-            item.render(&self.hbs)?;
-        }
+		let mut post_by_tag: HashMap<String, Vec<Post>> = HashMap::new();
+		for item in all_posts {
+			for tag in &item.tags {
+				if !post_by_tag.contains_key(tag) {
+					post_by_tag.insert(tag.to_string(), vec![]);
+				}
+				post_by_tag.get_mut(tag).unwrap().push(item.clone());
+			}
+			item.render(&self.hbs)?;
+		}
+		let tags_dir = self.dest_dir.join("tags");
+		if !tags_dir.exists() {
+			fs::create_dir(tags_dir)?;
+		}
 
-        let tags_dir = self.dest_dir.join("tags");
-        if !tags_dir.exists() {
-            fs::create_dir(tags_dir)?;
-        }
+		for (k, v) in &post_by_tag {
+			self.render_tags(
+				format!("tags/{}", k),
+				"tags",
+				&json!({"parent": "layout", "tag":k, "posts": v}),
+			)?;
+		}
 
-        for (k, v) in &post_by_tag {
-            self.render_tags(
-                format!("tags/{}", k),
-                "tags",
-                &json!({"parent": "layout", "tag":k, "posts": v}),
-            )?;
-        }
+		Ok(())
+	}
 
-        Ok(())
-    }
+	pub fn render(&self, file_path: &str) -> Result<(), RenderError> {
+		let new_path = Path::new(file_path);
+		let dest_file_name = new_path.file_stem().unwrap().to_str().unwrap();
+		let mut f_path = self.posts_dir.join(file_path);
+		f_path.set_extension("markdown");
+		let (_, contents) = self.parse_content(&f_path);
+		self.render_other(
+			dest_file_name,
+			&json!({"parent": "layout", "contents": contents}),
+		)?;
 
-    pub fn render(&self, file_path: &str) -> Result<(), RenderError> {
-        let new_path = Path::new(file_path);
-        let dest_file_name = new_path.file_stem().unwrap().to_str().unwrap();
-        let mut f_path = self.posts_dir.join(file_path);
-        f_path.set_extension("markdown");
-        let (_, contents) = self.parse_content(&f_path);
-        self.render_other(
-            dest_file_name,
-            &json!({"parent": "layout", "contents": contents}),
-        )?;
+		Ok(())
+	}
 
-        Ok(())
-    }
+	pub fn copy_static_files(src_dir: PathBuf, dest_dir: PathBuf) {
+		for entry in fs::read_dir(src_dir).unwrap() {
+			let entry_path = entry.unwrap().path();
+			let entry_path_name = entry_path.file_name().unwrap();
+			if entry_path.is_dir() {
+				let new_dir = dest_dir.join(entry_path_name);
+				Blogger::copy_static_files(entry_path, new_dir);
+			} else {
+				if !dest_dir.exists() {
+					fs::create_dir_all(&dest_dir).unwrap();
+				}
+				let new_file_path = dest_dir.join(entry_path_name);
+				fs::copy(&entry_path, &new_file_path).unwrap();
+			}
+		}
+	}
 
-    pub fn copy_static_files(src_dir: PathBuf, dest_dir: PathBuf) {
-        for entry in fs::read_dir(src_dir).unwrap() {
-            let entry_path = entry.unwrap().path();
-            let entry_path_name = entry_path.file_name().unwrap();
-            if entry_path.is_dir() {
-                let new_dir = dest_dir.join(entry_path_name);
-                Blogger::copy_static_files(entry_path, new_dir);
-            } else {
-                if !dest_dir.exists() {
-                    fs::create_dir_all(&dest_dir).unwrap();
-                }
-                let new_file_path = dest_dir.join(entry_path_name);
-                fs::copy(&entry_path, &new_file_path).unwrap();
-            }
-        }
-    }
+	fn load_posts(&self, exclude: &[String]) -> io::Result<Vec<Post>> {
+		let mut all_posts: Vec<Post> = vec![];
+		for entry in fs::read_dir(&self.posts_dir)? {
+			let entry_path = entry?.path();
+			let entry_ext = match entry_path.extension() {
+				Some(v) => v.to_str().unwrap().to_lowercase(),
+				_ => String::new(),
+			};
 
-    fn load_posts(&self, exclude: &[String]) -> io::Result<Vec<Post>> {
-        let mut all_posts: Vec<Post> = vec![];
-        for entry in fs::read_dir(&self.posts_dir)? {
-            let entry_path = entry?.path();
-            let entry_ext = match entry_path.extension() {
-                Some(v) => v.to_str().unwrap().to_lowercase(),
-                _ => String::new(),
-            };
+			let entry_name = match entry_path.file_stem() {
+				Some(v) => v.to_str().unwrap().to_string(),
+				_ => String::new(),
+			};
 
-            let entry_name = match entry_path.file_stem() {
-                Some(v) => v.to_str().unwrap().to_string(),
-                _ => String::new(),
-            };
+			if entry_path.is_file() && !exclude.contains(&entry_name) && entry_ext == "markdown" {
+				let (header, contents) = self.parse_content(&entry_path);
+				all_posts.push(Post::new(&self.dest_dir, header, entry_name, contents));
+			}
+		}
 
-            if entry_path.is_file() && !exclude.contains(&entry_name) && entry_ext == "markdown" {
-                let (header, contents) = self.parse_content(&entry_path);
-                all_posts.push(Post::new(&self.dest_dir, header, entry_name, contents));
-            }
-        }
+		Ok(all_posts)
+	}
 
-        Ok(all_posts)
-    }
+	fn parse_content(&self, entry_path: &PathBuf) -> (Header, String) {
+		let contents = fs::read_to_string(entry_path).unwrap();
+		if contents.starts_with("---") {
+			let end_of_yaml = contents[4..].find("---").unwrap() + 4;
+			let header = serde_yaml::from_str(&contents[..end_of_yaml]).unwrap();
+			let contents =
+				comrak::markdown_to_html(&contents[end_of_yaml + 5..], &self.comrak_options);
+			(header, contents)
+		} else {
+			(
+				Header::default(),
+				comrak::markdown_to_html(&contents, &self.comrak_options),
+			)
+		}
+	}
 
-    fn parse_content(&self, entry_path: &PathBuf) -> (Header, String) {
-        let contents = fs::read_to_string(entry_path).unwrap();
-        if contents.starts_with("---") {
-            let end_of_yaml = contents[4..].find("---").unwrap() + 4;
-            let header = serde_yaml::from_str(&contents[..end_of_yaml]).unwrap();
-            let contents =
-                comrak::markdown_to_html(&contents[end_of_yaml + 5..], &self.comrak_options);
-            (header, contents)
-        } else {
-            (
-                Header::default(),
-                comrak::markdown_to_html(&contents, &self.comrak_options),
-            )
-        }
-    }
+	fn render_other(&self, template_name: &str, data: &Value) -> Result<(), RenderError> {
+		let mut n_f = self.dest_dir.join(template_name);
+		n_f.set_extension("html");
 
-    fn render_other(&self, template_name: &str, data: &Value) -> Result<(), RenderError> {
-        let mut n_f = self.dest_dir.join(template_name);
-        n_f.set_extension("html");
+		let file = File::create(n_f).unwrap();
+		self.hbs.render_to_write(template_name, data, file)?;
 
-        let file = File::create(n_f).unwrap();
-        self.hbs.render_to_write(template_name, data, file)?;
+		Ok(())
+	}
 
-        Ok(())
-    }
+	fn render_tags(
+		&self,
+		parent_path: String,
+		template_name: &str,
+		data: &Value,
+	) -> Result<(), RenderError> {
+		let mut n_f = self.dest_dir.join(parent_path);
+		n_f.set_extension("html");
 
-    fn render_tags(
-        &self,
-        parent_path: String,
-        template_name: &str,
-        data: &Value,
-    ) -> Result<(), RenderError> {
-        let mut n_f = self.dest_dir.join(parent_path);
-        n_f.set_extension("html");
+		let file = File::create(n_f).unwrap();
+		self.hbs.render_to_write(template_name, data, file)?;
 
-        let file = File::create(n_f).unwrap();
-        self.hbs.render_to_write(template_name, data, file)?;
-
-        Ok(())
-    }
+		Ok(())
+	}
 }
--- src/post.rs
+++ src/post.rs
@@ -7,87 +7,94 @@ use serde_json::json;
 
 #[derive(Debug, Default, Serialize, Deserialize, Clone)]
 pub struct Post {
-    title: String,
-    pub created_date_time: String,
-    parent_dir: PathBuf,
-    pub dir: String,
-    file_name: String,
-    contents: String,
-    pub tags: Vec<String>,
+	title: String,
+	pub created_date_time: String,
+	parent_dir: PathBuf,
+	pub dir: String,
+	file_name: String,
+	contents: String,
+	pub tags: Vec<String>,
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
 pub struct Header {
-    title: String,
-    date_time: String,
-    tags: String,
+	title: String,
+	date_time: String,
+	tags: String,
+}
+
+impl Header {
+	pub fn generate_url(&self) -> String {
+		let date = self.date_time.split_whitespace().next().unwrap();
+		let v: Vec<&str> = date.split('-').collect();
+		format!("{}/{}/{}", v[0], v[1], v[2])
+	}
+
+	pub fn build_tags(&self) -> Vec<String> {
+		self.tags
+			.split_whitespace()
+			.map(|x| x.to_string())
+			.collect()
+	}
 }
 
 impl Post {
-    pub fn new(parent_dir: &Path, header: Header, file_name: String, contents: String) -> Post {
-        let mut split = header.date_time.split_whitespace();
-        let date = split.next().unwrap();
-        let v: Vec<&str> = date.split('-').collect();
+	pub fn new(parent_dir: &Path, header: Header, file_name: String, contents: String) -> Post {
+		Post {
+			title: header.title.clone(),
+			created_date_time: header.date_time.clone(),
+			parent_dir: parent_dir.to_path_buf(),
+			dir: header.generate_url(),
+			file_name,
+			contents,
+			tags: header.build_tags(),
+		}
+	}
 
-        Post {
-            title: header.title,
-            created_date_time: header.date_time.to_string(),
-            parent_dir: parent_dir.to_path_buf(),
-            dir: format!("{}/{}/{}", v[0], v[1], v[2]),
-            file_name: file_name,
-            contents,
-            tags: header
-                .tags
-                .split_whitespace()
-                .map(|x| x.to_string())
-                .collect(),
-        }
-    }
+	pub fn render(&self, hbs: &Handlebars) -> Result<(), RenderError> {
+		let file_dir = self.parent_dir.join(&self.dir);
+		fs::create_dir_all(&file_dir).unwrap();
 
-    pub fn render(&self, hbs: &Handlebars) -> Result<(), RenderError> {
-        let file_dir = self.parent_dir.join(&self.dir);
-        fs::create_dir_all(&file_dir).unwrap();
-
-        let mut f = file_dir.join(&self.file_name);
-        f.set_extension("html");
-        let file = File::create(f).unwrap();
-        hbs.render_to_write(
-            "post",
-            &json!({
+		let mut f = file_dir.join(&self.file_name);
+		f.set_extension("html");
+		let file = File::create(f).unwrap();
+		hbs.render_to_write(
+			"post",
+			&json!({
                 "parent": "layout",
                 "post": self}),
-            file,
-        )?;
+			file,
+		)?;
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 #[test]
 fn test_new_header() {
-    let header = Header {
-        title: String::from("test"),
-        date_time: String::from("aa"),
-        tags: String::from("hello world"),
-    };
-    let v1: Vec<&str> = header.tags.split_whitespace().collect();
-    assert_eq!("hello", v1[0]);
-    assert_eq!("world", v1[1]);
+	let header = Header {
+		title: String::from("test"),
+		date_time: String::from("aa"),
+		tags: String::from("hello world"),
+	};
+	let v1: Vec<&str> = header.tags.split_whitespace().collect();
+	assert_eq!("hello", v1[0]);
+	assert_eq!("world", v1[1]);
 }
 
 #[test]
 fn test_new_post() {
-    let header = Header {
-        title: String::from("test"),
-        date_time: String::from("2019-2-25"),
-        tags: String::from("hello world"),
-    };
-    let post = Post::new(
-        Path::new("a"),
-        header,
-        String::from("ssssss"),
-        String::from("sss"),
-    );
-    assert_eq!("test", post.title);
-    assert_eq!("world", post.tags[1]);
+	let header = Header {
+		title: String::from("test"),
+		date_time: String::from("2019-2-25"),
+		tags: String::from("hello world"),
+	};
+	let post = Post::new(
+		Path::new("a"),
+		header,
+		String::from("ssssss"),
+		String::from("sss"),
+	);
+	assert_eq!("test", post.title);
+	assert_eq!("world", post.tags[1]);
 }
